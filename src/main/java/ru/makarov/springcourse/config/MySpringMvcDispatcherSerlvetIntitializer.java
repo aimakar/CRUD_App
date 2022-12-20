9da5a872e4ee37ea639cfa7d6c0c6ded2fb649b1
Andrey <aimakar@mail.ru>
2022-12-20 11:43:46 +0300

    Lesson 21

--- src/main/java/ru/makarov/springcourse/config/MySpringMvcDispatcherSerlvetIntitializer.java
+++ src/main/java/ru/makarov/springcourse/config/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -1,6 +1,23 @@
-package ru.makarov.springcourse.config;/** 
- *
+package ru.makarov.springcourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
  * @author Andrey Makarov on 20.12.2022
  */
-public class MySpringMvcDispatcherSerlvetIntitializer {
+public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
