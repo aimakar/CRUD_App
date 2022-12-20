46c5a650f60fb160de133ec3e0ea4765e34dc201
Andrey <aimakar@mail.ru>
2022-12-20 14:34:18 +0300

    Lesson 23

--- src/main/java/ru/makarov/springcourse/config/MySpringMvcDispatcherSerlvetIntitializer.java
+++ src/main/java/ru/makarov/springcourse/config/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -1,6 +1,10 @@
 package ru.makarov.springcourse.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 /**
  * @author Andrey Makarov on 20.12.2022
@@ -20,4 +24,16 @@ public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotation
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
+    }
+
 }
