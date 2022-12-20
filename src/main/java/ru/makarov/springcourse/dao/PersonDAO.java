42ac4afaa5f90bf97861377a065c4d69e87464c0
Andrey <aimakar@mail.ru>
2022-12-20 16:33:40 +0300

    Lesson 24

--- src/main/java/ru/makarov/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/makarov/springcourse/dao/PersonDAO.java
@@ -18,10 +18,10 @@ public class PersonDAO {
     {
         people = new ArrayList<>();
 
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 23, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 35, "bobd@ya.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 65, "mkinderq@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 35, "katya@yahoo.com"));
     }
 
     public List<Person> index() {
@@ -40,6 +40,8 @@ public class PersonDAO {
     public void update(int id, Person person) {
         Person personToBeUpdated = show(id);
         personToBeUpdated.setName(person.getName());
+        personToBeUpdated.setAge(person.getAge());
+        personToBeUpdated.setEmail(person.getEmail());
     }
 
     public void delete(int id) {
